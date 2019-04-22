d218b8adfcdac00dcf12cab5a35233c8a827463c
Den Kim <d.kim@gpsolutions.com>
2019-04-22 13:51:01 +0300

    first commit

---
+++ src/main/java/com/darseg/justPokeABeer/JustPokeABeerApplication.java
@@ -0,0 +1,13 @@
+package com.darseg.justPokeABeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustPokeABeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustPokeABeerApplication.class, args);
+	}
+
+}
