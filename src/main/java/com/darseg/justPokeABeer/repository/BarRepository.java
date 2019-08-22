a0a50aeaffbcb3eb3bdc169ee78394bcd7b225ed
Dars <den.v.kim@gmail.com>
2019-08-22 22:54:32 +0300

    new

--- src/main/java/com/darseg/justPokeABeer/repository/BarRepository.java
+++ src/main/java/com/darseg/justPokeABeer/repository/BarRepository.java
@@ -2,14 +2,20 @@ package com.darseg.justPokeABeer.repository;
 
 import com.darseg.justPokeABeer.dbo.BarDBO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.util.List;
 
 @Transactional
+@Repository
 public interface BarRepository extends JpaRepository<BarDBO, Long> {
 	
 	BarDBO getByBid(Long bid);
-	
-	
+
+	@Override
+	BarDBO save(BarDBO s);
+
+	@Override
+	<S extends BarDBO> List<S> saveAll(Iterable<S> iterable);
 }
